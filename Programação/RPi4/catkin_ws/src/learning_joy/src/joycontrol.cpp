2867d6b0d4d52ba6fbffb41d4a78aea3b3d5a3e7
Rodri2B <rodrigocbastos@usp.br>
2024-07-08 17:00:38 -0300

    teleop

--- Programação/RPi4/catkin_ws/src/learning_joy/src/joycontrol.cpp
+++ Programação/RPi4/catkin_ws/src/learning_joy/src/joycontrol.cpp
@@ -3,43 +3,42 @@
 #include <sensor_msgs/Joy.h>
 
 
-class TeleopTurtle
+class TeleopJoy
 {
 public:
-  TeleopTurtle();
+  TeleopJoy();
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
   ros::NodeHandle nh_;
 
-  int linear_, angular_;
-  double l_scale_, a_scale_;
+  //int linear_, angular_;
+  double joy_scale, left_scale_trigger,right_scale_trigger;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
 
 };
 
 
-TeleopTurtle::TeleopTurtle():
-  linear_(1),
-  angular_(2)
+TeleopJoy::TeleopJoy()//:
+  //linear_(1),
+  //angular_(2)
 {
 
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
+  nh_.param("joy_params/joy_scale", joy_scale);
+  nh_.param("joy_params/left_scale_trigger", left_scale_trigger);
+  nh_.param("joy_params/right_scale_trigger", right_scale_trigger);
 
 
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
 
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopJoy::joyCallback, this);
 
 }
 
-void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+void TeleopJoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   geometry_msgs::Twist twist;
   twist.angular.z = a_scale_*joy->axes[angular_];
@@ -50,8 +49,8 @@ void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "teleop_turtle");
-  TeleopTurtle teleop_turtle;
+  ros::init(argc, argv, "joy_teleop");
+  TeleopJoy teleop;
 
   ros::spin();
 }
